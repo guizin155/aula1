ca03f4c0c488782e81260155f234085c1648a714
guizin155 <guilhermecosta021freitas@gmail.com>
2025-04-01 08:41:03 -0300

    inicializando projeto

---
+++ aula.js
@@ -0,0 +1,25 @@
+// (Mostrar e Ocultar)
+
+
+// function toggleVisibilidade() {
+//     let paragrafo = document.getElementById("meuParagrafo");
+//     paragrafo.style.display = paragrafo.style.display === 'none' ? 'block' : 'none';
+// }
+
+
+
+// (Formulário de Validação Simples)
+
+
+// function validarFormulario() {
+//     let campo = document.getElementById("campoTexto").value;
+//     let mensagemErro = document.getElementById("mensagemErro");
+    
+//     if (campo.trim() === "") {
+//         mensagemErro.style.display = "block";
+//         return false;
+//     } else {
+//         mensagemErro.style.display = "none";
+//         return true;
+//     }
+// }
